6bb41ff57bf371f5ca228c52da4c43ad4cd0b6cb
chenterphai <chenterphai@gmail.com>
2026-08-28 16:23:19 +0700

    fix: remove proxy url

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -2,14 +2,13 @@ import { ClerkProvider } from '@clerk/react-router';
 import { Outlet } from 'react-router';
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-const PROXY_URL = import.meta.env.DEV ? undefined : import.meta.env.VITE_CLERK_PROXY_URL;
 if (!PUBLISHABLE_KEY) {
   throw new Error('Add your Clerk Publishable Key to the .env file');
 }
 
 export default function RootLayout() {
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" proxyUrl={PROXY_URL}>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
       <Outlet />
     </ClerkProvider>
   );
